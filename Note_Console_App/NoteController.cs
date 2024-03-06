6ecc683d50c0680927c1384c99e3d716f51525be
Alex Forostianov <bezdelinik.1@gmail.com>
2024-03-06 18:50:08 +0100

    Addet Controller

---
+++ Note_Console_App/NoteController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note_Console_App
+{
+    internal class NoteController
+    {
+        public int UserId { get; set; }
+        public int UserAge { get; set; }
+    }
+}
